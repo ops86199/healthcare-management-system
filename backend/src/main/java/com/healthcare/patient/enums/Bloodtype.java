825727b42350a0773e133ba7b717b66deb50bcdc
ops86199 <swainomprakash12@gmail.com>
2026-05-01 18:06:57 +0530

    change blodetype

--- backend/src/main/java/com/healthcare/patient/enums/Bloodtype.java
+++ backend/src/main/java/com/healthcare/patient/enums/Bloodtype.java
@@ -1,6 +1,6 @@
 package com.healthcare.patient.enums;
 
-public enum BloodType {
+public enum Bloodtype {
     A_POSITIVE("A+"), A_NEGATIVE("A-"),
     B_POSITIVE("B+"), B_NEGATIVE("B-"),
     AB_POSITIVE("AB+"), AB_NEGATIVE("AB-"),
